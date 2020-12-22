388fa18a2d8df6106de7be65c5e4444e6b522e7a
13RuN <runemal13@gmail.com>
2020-12-22 15:04:57 +0500

    ver 0.1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,45 @@
 class ArticlesController < ApplicationController
-
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  
   def new
   end
 
   def create
-    
-    render plain: params[:article].inspect
-    
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  def index
+    @articles = Article.all.reverse
+  end
+
+  def show
+  end
+
+  def edit
+  end 
+  
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_103637) do
+ActiveRecord::Schema.define(version: 2020_12_18_095002) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
