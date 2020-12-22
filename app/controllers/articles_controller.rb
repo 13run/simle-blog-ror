20439fb1a98289b843a691d040329f77fd50a802
13RuN <runemal13@gmail.com>
2020-12-22 15:39:12 +0500

    add delete & edit btns for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path 
+  end
+  
+
   private
 
   def article_params
