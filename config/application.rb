917f607a704292c63d1841302eade089ee60ae12
13RuN <runemal13@gmail.com>
2020-12-25 18:07:57 +0500

    add devise

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.i18n.default_locale = :ru
     config.load_defaults 6.1
-   
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
+  devise_for :users
+  root to: 'home#index'
   get 'about' => 'pages#about'
   get 'terms' => 'pages#terms'
-
+  get '/' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :contacts, only: [:new, :create], path_names: { new: '' } 
   resources :articles do
